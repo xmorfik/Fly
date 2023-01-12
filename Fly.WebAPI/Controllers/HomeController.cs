120bb84ea21be8b1e0c6da5c3ed5dd8207b4c996
Andrii <a.dukhno@outlook.com>
2023-01-12 12:00:25 +0200

    Add project files.

---
+++ Fly.WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fly.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
