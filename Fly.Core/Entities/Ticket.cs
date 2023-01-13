7e4dda1dd55e8ea20e93506d7c1f513dc195ab24
Andrii <a.dukhno@outlook.com>
2023-01-13 11:06:00 +0200

    add isSold

--- Fly.Core/Entities/Ticket.cs
+++ Fly.Core/Entities/Ticket.cs
@@ -5,4 +5,5 @@ public class Ticket : BaseEntity
     public decimal Price { get; set; }
     public Flight Flight { get; set; }
     public Seat Seat { get; set; }
+    public bool IsSold { get; set; }
 }
