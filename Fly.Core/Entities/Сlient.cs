13bceb889cf7f3670864bd62f5dcb7146cd9770d
Andrii Dukhno <a.dukhno@outlook.com>
2023-01-14 14:03:26 +0200

    add client entity

---
+++ Fly.Core/Entities/Сlient.cs
@@ -0,0 +1,15 @@
+﻿namespace Fly.Core.Entities;
+
+public class Сlient: BaseEntity
+{
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public ICollection<Ticket>? Tickets { get; set; }
+
+}
