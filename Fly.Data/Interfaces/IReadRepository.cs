4a6b06a488d5bb87eb218339ee35137a500432dd
Andrii Dukhno <a.dukhno@outlook.com>
2023-01-15 13:56:07 +0200

    add repos interfaces

---
+++ Fly.Data/Interfaces/IReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.Specification;
+using Fly.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fly.Data.Interfaces
+{
+    public interface IReadRepository<T> : IReadRepositoryBase<T> where T : class, IAggregateRoot
+    {
+    }
+}
---
+++ Fly.Data/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.Specification;
+using Fly.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fly.Data.Interfaces
+{
+    public interface IRepository<T> : IRepositoryBase<T> where T : class, IAggregateRoot
+    {
+    }
+}
