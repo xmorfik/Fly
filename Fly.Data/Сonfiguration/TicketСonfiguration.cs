be784c2cb20e44b43f6e09a78a2e521ec31f7140
Andrii Dukhno <a.dukhno@outlook.com>
2023-01-16 17:42:41 +0200

    fix encoding

--- Fly.Data/Сonfiguration/TicketСonfiguration.cs
+++ Fly.Data/Сonfiguration/TicketСonfiguration.cs
@@ -2,7 +2,7 @@ using Fly.Core.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Fly.Data.Ñonfiguration;
+namespace Fly.Data.Configuration;
 
 public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
 {
